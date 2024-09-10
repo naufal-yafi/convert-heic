1f17ce613a54ec219f45dcebf77dbf5b0eefa5b6
naufal-yafi <muhammadnaufalyafi00@gmail.com>
2024-09-10 13:48:53 +0000

    feat: terminal

---
+++ src/terminal.js
@@ -0,0 +1,51 @@
+export class Terminal {
+  static help() {
+    console.log(
+      '\n------------------------------------------------------------',
+    );
+    console.log('                    AVAILABLE COMMANDS');
+    console.log('------------------------------------------------------------');
+    console.log('node app <...commands>');
+
+    console.log(
+      `${this.color(
+        'yellow',
+        '-h --help',
+      )}                        : show available commands`,
+    );
+
+    console.log(
+      `${Terminal.color(
+        'green',
+        'duplicate',
+      )} <..ext> <..count>      : duplicate files`,
+    );
+    console.log('------------------------------------------------------------');
+  }
+
+  static color(color, text) {
+    if (color === 'red') {
+      return `\x1b[31m${text}\x1b[0m`;
+    } else if (color === 'green') {
+      return `\x1b[32m${text}\x1b[0m`;
+    } else if (color === 'yellow') {
+      return `\x1b[33m${text}\x1b[0m`;
+    } else {
+      return text;
+    }
+  }
+
+  static error(message) {
+    console.error(`${this.color('red', 'Err')}: ${message}`);
+    console.info(
+      `Type ${this.color('green', '-h')} or ${this.color(
+        'green',
+        '--help',
+      )} to show available commands\n`,
+    );
+  }
+
+  static exit() {
+    process.exit(0);
+  }
+}
